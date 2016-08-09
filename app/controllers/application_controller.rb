a17280193bd93cb63cb4c79026412fd7b3a791fa
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-08-10 08:59:35 +1000

    Add styling and split out files, add local prefixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def self.local_prefixes
+    ['layouts', controller_path]
+  end
+  private_class_method :local_prefixes
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,9 @@ class StaticPagesController < ApplicationController
   def show
     render params[:id]
   end
+
+  def self.local_prefixes
+    [controller_path]
+  end
+  private_class_method :local_prefixes
 end
