387c69800dbb85e0d5c2163aeab36ee7307b5d83
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-08-14 18:49:20 +1000

    Minor route updating

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "home", to: "pages#show", id: "home"
   get "help", to: "pages#show", id: "help"
   get "about", to: "pages#show", id: "about"
   get "contact", to: "pages#show", id: "contact"
