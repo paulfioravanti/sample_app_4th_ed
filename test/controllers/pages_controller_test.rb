ec5e58531b7f9b6648a25e5221f089aee9cb3ee7
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-08-20 11:24:01 +1000

    Minor refactor

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  attr_reader :base_title
+
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
@@ -8,24 +10,24 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", @base_title
+    assert_select "title", base_title
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | #{base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | #{base_title}"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact | #{base_title}"
   end
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
+  attr_reader :string
+
   def setup
     @string = "Help"
   end
@@ -8,8 +10,8 @@ class ApplicationHelperTest < ActionView::TestCase
   test "#full_title" do
     assert_equal full_title, "Ruby on Rails Tutorial Sample App"
     assert_equal(
-      full_title(@string),
-      "#{@string} | Ruby on Rails Tutorial Sample App"
+      full_title(string),
+      "#{string} | Ruby on Rails Tutorial Sample App"
     )
   end
 end
