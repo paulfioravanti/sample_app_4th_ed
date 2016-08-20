1228aafbc9eb717ce9a4b9cb734987a11e2407d3
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-08-20 16:51:11 +1000

    Attempt to use SimpleCov locally, but Code Climate on CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,19 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if ENV["TRAVIS"]
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+else
+  require "simplecov"
+  SimpleCov.start "rails"
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
+SimpleCov.minimum_coverage 100
+SimpleCov.refuse_coverage_drop
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
