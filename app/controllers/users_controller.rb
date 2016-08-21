fa988aaadbe4627b81a6ff4383194ff13cd6aa4d
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-08-21 19:14:02 +1000

    Create signup form visible on the page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,26 @@ class UsersController < ApplicationController
   def show
     @user = UserDecorator.decorate(User.find(params[:id]))
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password, :password_confirmation
+    )
+  end
+
+  def self.local_prefixes
+    [controller_path]
+  end
+  private_class_method :local_prefixes
 end
