72512270e28248262c460151a901d14c43414992
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-08-22 08:42:15 +1000

    Finish up to 8.2.2 Current user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if user = User.find_and_authenticate(params[:session])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "Invalid email/password combination"
+      render "new"
+    end
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+
   get "help", to: "pages#show", id: "help"
   get "about", to: "pages#show", id: "about"
   get "contact", to: "pages#show", id: "contact"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
   get "signup", to: "users#new"
   post "signup", to: "users#create"
