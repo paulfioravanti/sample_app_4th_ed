7658f8f68fba8ee53b1c92c1ae5f49530ee15474
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-09-24 22:27:37 +1000

    Extract hash digest method into test directory

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,7 @@ require "rails/test_help"
 require "minitest/reporters"
 require "minitest/mock"
 Minitest::Reporters.use!
+Dir[Rails.root.join("test/support/**/*.rb")].each { |file| require file }
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
