3067505306ce66273a13b6b76e5e957f6ef65742
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-01 14:19:20 +1000

    Minor test refactoring

--- test/services/password/digest/matches_token_test.rb
+++ test/services/password/digest/matches_token_test.rb
@@ -1,51 +1,45 @@
 require "test_helper"
 
 class Password::Digest::MatchesTokenTest < ActiveSupport::TestCase
-  attr_reader :token, :actual
+  attr_reader :token, :digest, :actual, :expected
 
   def setup
     @token = "password"
     @actual = -> { Password::Digest.matches_token?(digest, token) }
   end
 
-  class DigestIsPasswordTest < Password::Digest::MatchesTokenTest
-    attr_reader :digest
-
+  class DigestIsPasswordTest < self
     def setup
       super
       @digest = "$2a$10$s0NLf4rIfAOE8HAl9pVYG.sza43aSstcjiIUur6Xjslw/k.KGYmbK"
+      @expected = true
     end
 
     test ".matches_token?" do
-      expected = true
       assert_equal expected, actual.call
     end
   end
 
-  class DigestIsNotPasswordTest < Password::Digest::MatchesTokenTest
-    attr_reader :digest
-
+  class DigestIsNotPasswordTest < self
     def setup
       super
       @digest = "$2a$10$N3NMcb7r9oYQKZtlA7KkEu2YTks0M33lLlz0DrxuOIkmbk9Mi2C8W"
+      @expected = false
     end
 
     test ".matches_token?" do
-      expected = false
       assert_equal expected, actual.call
     end
   end
 
-  class DigestBlankTest < Password::Digest::MatchesTokenTest
-    attr_reader :digest
-
+  class DigestBlankTest < self
     def setup
       super
       @digest = nil
+      @expected = false
     end
 
     test ".matches_token?" do
-      expected = false
       assert_equal expected, actual.call
     end
   end
