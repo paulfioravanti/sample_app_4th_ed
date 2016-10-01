6b32c7d798bc1695e5734a75a3e112623ed34d6b
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-01 23:13:58 +1000

    Remove extraneous files

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,15 +3,4 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: "Account activation"
   end
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
 end
