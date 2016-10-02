64d99d984496011ddea0eb3cd3e5e34bb8e28b95
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-03 08:48:03 +1100

    Complete up to 12.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,6 +80,11 @@ class User < ApplicationRecord
     )
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
   def downcase_email
