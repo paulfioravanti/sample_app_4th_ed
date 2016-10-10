02a8a56e293981a85ae35798a3da36d0667d9f33
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-10 21:45:46 +1100

    Fix javascript comment

--- app/assets/javascripts/microposts.js
+++ app/assets/javascripts/microposts.js
@@ -1,6 +1,6 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
 $("#micropost_picture").bind("change", function() {
   var size_in_megabytes = this.files[0].size/1024/1024;
   if (size_in_megabytes > 5) {
