3d4cb3678d2e3341c7d2653223f64bba2d76e7a4
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-10 22:40:38 +1100

    Change javascript alert to a flash-like thing

--- app/assets/javascripts/microposts.js
+++ app/assets/javascripts/microposts.js
@@ -1,9 +1,18 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://coffeescript.org/
-$("#micropost_picture").bind("change", function() {
-  var size_in_megabytes = this.files[0].size/1024/1024;
-  if (size_in_megabytes > 5) {
-    alert("Maximum file size is 5MB. Please choose a smaller file.");
+$(document).ready(function() {
+  if ($("#micropost_picture").length) {
+    $("#micropost_picture").bind("change", function() {
+      var size_in_megabytes = this.files[0].size / 1024 / 1024;
+
+      if (size_in_megabytes > 5) {
+        $(".new_micropost").prepend(
+          $("<div>", { class: "error-explanation" }).append(
+            $("<div>", {
+              class: "alert alert-danger",
+              text: "Maximum file size is 5MB. Please choose a smaller file."
+            })
+          )
+        );
+      }
+    });
   }
 });
