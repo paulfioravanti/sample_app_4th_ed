b108937fd2714b3e847bfcc42d2459ecc2bfd307
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-16 23:19:04 +1100

    Remove show_follow from users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update]
   before_action :active_user, only: :show
   before_action :correct_user, only: [:edit, :update]
@@ -48,25 +48,7 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  def following
-    @title = "Following"
-    show_follow(:following)
-  end
-
-  def followers
-    @title = "Followers"
-    show_follow(:followers)
-  end
-
   private
-
-  def show_follow(relationship)
-    @user  = UserDecorator.decorate(User.find(params[:id]))
-    @users = UserDecorator.decorate_collection(
-      @user.public_send(relationship).paginate(page: params[:page])
-    )
-    render "show_follow"
-  end
 
   def set_user
     @user =
