00de42662ce50da0b078a829cf7816cc8dcaf34b
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-16 17:52:07 +1100

    Complete up to 14.2.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :set_user, only: [:show, :edit, :update]
   before_action :active_user, only: :show
   before_action :correct_user, only: [:edit, :update]
@@ -46,6 +46,24 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user  = UserDecorator.decorate(User.find(params[:id]))
+    @users = UserDecorator.decorate_collection(
+      @user.following.paginate(page: params[:page])
+    )
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = UserDecorator.decorate(User.find(params[:id]))
+    @users = UserDecorator.decorate_collection(
+      @user.followers.paginate(page: params[:page])
+    )
+    render 'show_follow'
   end
 
   private
