f8260952a75b4d5f6432d095d01b017ffdbb5e5d
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-10-21 11:13:21 +1100

    Minor fix

--- app/queries/micropost_feed.rb
+++ app/queries/micropost_feed.rb
@@ -2,13 +2,13 @@ module MicropostFeed
   module_function
 
   def for(user)
-    microposts_by(followed_user_ids_for(user)).or(microposts_by(user))
+    microposts_by(followed_users_for(user)).or(microposts_by(user))
   end
 
-  def followed_user_ids_for(user)
+  def followed_users_for(user)
     Relationship.where(follower: user).pluck(:followed_id)
   end
-  private_class_method
+  private_class_method :followed_users_for
 
   def microposts_by(users)
     Micropost.includes(:user).where(user: users)
